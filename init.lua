b53257314f4de50acea109f3c2ff236d6eb96a4c
Debian <debian@vps-fbb5e062.vps.ovh.net>
2025-12-15 21:06:51 +0000

    Added personnal keymap for LSP code check.

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 require("config.lazy")
 
+vim.keymap.set('n', '<space>n', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>a', vim.lsp.buf.code_action)
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.api.nvim_create_autocmd('FileType', {
